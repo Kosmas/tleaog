c15dc5e7b6791e31d999d24f7900f83064c830ee
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-09-12 20:15:09 +0200

    implementing the handle_call callback

--- chapter_4/metex/lib/worker.ex
+++ chapter_4/metex/lib/worker.ex
@@ -17,5 +17,14 @@ defmodule Metex.Worker do
     {:ok, %{}}
   end
 
+  def handle_call({:location, location}, _from, state) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}C", new_stats}
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
   ## Helper Functions
 end
