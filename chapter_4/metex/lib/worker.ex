22c260c1e658d7e1ac4d1379b08850ac520ad4f0
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-09-17 23:50:05 +0200

    implementing the helper functions - Listing 4.9

--- chapter_4/metex/lib/worker.ex
+++ chapter_4/metex/lib/worker.ex
@@ -17,7 +17,7 @@ defmodule Metex.Worker do
     {:ok, %{}}
   end
 
-  def handle_call({:location, location}, _from, state) do
+  def handle_call({:location, location}, _from, stats) do
     case temperature_of(location) do
       {:ok, temp} ->
         new_stats = update_stats(stats, location)
@@ -26,5 +26,40 @@ defmodule Metex.Worker do
         {:reply, :error, stats}
     end
   end
+
   ## Helper Functions
+
+  defp temperature_of(location) do
+    url_for(location) |> HTTPoison.get |> parse_response
+  end
+
+  defp url_for(location) do
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
+  end
 end
