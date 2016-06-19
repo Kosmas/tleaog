ddb16fcb74f7841e82bf69680ff2ae710119032f
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-06-19 16:01:02 +0200

    upgrade elixir version to 1.2.6

--- chapter_4/metex/mix.exs
+++ chapter_4/metex/mix.exs
@@ -4,7 +4,7 @@ defmodule Metex.Mixfile do
   def project do
     [app: :metex,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
