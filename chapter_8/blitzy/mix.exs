7f537ed4cb8f0eaee75cc137f03eb25aa96dffe1
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-06-26 17:29:29 +0200

    add dependencies

--- chapter_8/blitzy/mix.exs
+++ chapter_8/blitzy/mix.exs
@@ -4,7 +4,7 @@ defmodule Blitzy.Mixfile do
   def project do
     [app: :blitzy,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.2.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -14,7 +14,8 @@ defmodule Blitzy.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [mod: {Blitzy, []},
+     applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +28,9 @@ defmodule Blitzy.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.9.0"},
+      {:timex,     "~> 2.2.1"}
+    ]
   end
 end
