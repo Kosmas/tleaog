53f58fadb2bd59028295848fe9aad5712b17c9f2
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-06-26 20:04:10 +0200

    8.4 - modifying the worker process so that it can send its results to its caller process

--- chapter_8/blitzy/lib/blitzy/worker.ex
+++ chapter_8/blitzy/lib/blitzy/worker.ex
@@ -2,9 +2,9 @@ defmodule Blitzy.Worker do
   use Timex
   require Logger
 
-  def start(url) do
-    {timestamp, response} = Time.measure(fn -> HTTPoison.get(url) end)
-    handle_response =({Time.to_msecs(timestamp), response})
+  def start(url, caller, func \\ &HTTPoison.get/1) do
+    {timestamp, response} = Time.measure(fn -> func.(url) end)
+    caller |> send({self, handle_response({Time.to_msecs(timestamp), response})})
   end
 
   defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}}) when code >= 200 and code <= 304 do
